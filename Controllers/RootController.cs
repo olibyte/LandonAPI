dc9f480ee9350569d86920017e89ad2f68b38b0f
Oliver <ocben1@gmail.com>
2019-10-22 08:21:13 +1100

    create a root controller

---
+++ Controllers/RootController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonAPI.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController :  ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+
+            return Ok(response);
+        }
+    }
+}
