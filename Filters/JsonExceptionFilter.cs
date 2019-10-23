509a3ef80de26199acb0aefd1f79d01942036476
Oliver <ocben1@gmail.com>
2019-10-24 08:42:28 +1100

    jsonexceptionfilter

---
+++ Filters/JsonExceptionFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonAPI.Filters
+{
+    public class JsonExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var error = new ApiError();
+        }
+    }
+}
