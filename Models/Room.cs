df308b021158fb6fa71645915bd5d2d1d377d77d
Oliver <ocben1@gmail.com>
2019-10-25 21:33:15 +1100

    create data model classes

---
+++ Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonAPI.Models
+{
+    public class Room : Resource
+    {
+        public string Name { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
