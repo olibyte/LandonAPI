a34635c7b5e92c4699413a77b39bdbd3f1b7fb6c
Oliver <ocben1@gmail.com>
2019-10-25 20:15:55 +1100

    create a resource class

---
+++ Models/HotelInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonAPI.Models
+{
+    public class HotelInfo : Resource
+    {
+        public string Title { get; set; }
+        public string Tagline { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public Address Location { get; set; }
+    }
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ Models/Resource.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonAPI.Models
+{
+    public abstract class Resource
+    {
+        [JsonProperty(Order= -2)]
+        public string Href { get; set; }
+    }
+}
