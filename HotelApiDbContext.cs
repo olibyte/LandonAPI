6176c1c9f0d5656542b20d9f9c75732a3425f54c
Oliver <ocben1@gmail.com>
2019-10-25 21:47:50 +1100

    seed database with test data

--- HotelApiDbContext.cs
+++ HotelApiDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LandonAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ SeedData.cs
@@ -0,0 +1,41 @@
+﻿using LandonAPI.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonAPI
+{
+    public static class SeedData
+    {
+        public static async Task InitializeAsync(IServiceProvider services)
+        {
+            await AddTestData(
+                services.GetRequiredService<HotelApiDbContext>());
+        }
+        public static async Task AddTestData(HotelApiDbContext context)
+        {
+            if (context.Rooms.Any())
+            {
+                //Already has data
+                return;
+            }
+            context.Rooms.Add(new RoomEntity
+            {
+                Id = Guid.Parse("301df04d-8679-4b1b-ab92-0a586ae53d08"),
+                Name = "Oxford Suite",
+                Rate = 10119,
+            });
+
+            context.Rooms.Add(new RoomEntity
+            {
+                Id = Guid.Parse("ee2b83be-91db-4de5-8122-35a9e9195976"),
+                Name = "Driscoll Suite",
+                Rate = 23959
+            });
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
