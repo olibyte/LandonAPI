ba8055f39e0a38189dc438604c31e4ad4d728aa8
Oliver <ocben1@gmail.com>
2019-10-25 22:04:12 +1100

    return a resource from a controller

--- Controllers/RoomsController.cs
+++ Controllers/RoomsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LandonAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,38 @@ namespace LandonAPI.Controllers
     [ApiController]
     public class RoomsController : ControllerBase
     {
+        private readonly HotelApiDbContext _context;
+
+        public RoomsController(HotelApiDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet(Name = nameof(GetRooms))]
         public IActionResult GetRooms()
         {
             throw new NotImplementedException();
+        }
+        //GET /rooms/{roomId}
+        [HttpGet("{roomId}", Name = nameof(GetRoomById))]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<Room>> GetRoomById(Guid roomId)
+        {
+            var entity = await _context.Rooms
+                .SingleOrDefaultAsync(x => x.Id == roomId);
+            if(entity == null)
+            {
+                return NotFound();
+            }
+
+            var resource = new Room
+            {
+                Href = Url.Link(nameof(GetRoomById), new { roomId = entity.Id }),
+                Name = entity.Name,
+                Rate = entity.Rate / 100.0m
+            };
+
+            return resource;
         }
     }
 }
